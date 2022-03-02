ad35848d761545a0caa1c57362bab85ee0302c79
AmolDhait <amol.dhait97@gmail.com>
2022-03-02 10:51:38 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wlecome to Employee Wage Program");
+            EmpAttCheck.Check_Emp_Presence();
+            //EmpWage.CalculateEmpWage();
+            EmpWagesBySwitchCase.CalculateEmpWage();
+        }
+    }
+}
